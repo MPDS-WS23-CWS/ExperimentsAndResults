ba50aeb2979c836c7f2a75b3e781e366a407a427
Niklasfomin <niklas.fomin@campus.tu-berlin.de>
2023-12-21 09:53:29 +0100

    added metrics and db setup into setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
 namespace="cws"
-
+namespace2="default"
+# setup of cws related stuff
 kubectl create namespace $namespace
 kubectl apply -f setup/pv.yaml --namespace $namespace
 kubectl apply -f setup/pvc.yaml --namespace $namespace
 kubectl apply -f setup/management.yaml --namespace $namespace
 kubectl wait --for=condition=ready pod management --namespace $namespace
 
-#kubectl cp inputs/rnaseq $namespace/management:/input/
+# setup metricScraper
+# 1 basic prometheus setup
+kubectl create namespace monitoring
+kubectl create -f ../ProvenanceEngine/MetricScraper/minimal_setup/kubernetes-prometheus/clusterRole.yaml
+kubectl create -f ../ProvenanceEngine/MetricScraper/minimal_setup/kubernetes-prometheus/config-map.yaml
+kubectl create -f ../ProvenanceEngine/MetricScraper/minimal_setup/kubernetes-prometheus/prometheus-deployment.yaml
 
+# 2 kube-state-metrics
+kubectl apply -f ../ProvenanceEngine/MetricScraper/minimal_setup/kube-state-metrics
+
+# 3 node-exporter
+kubectl create -f ../ProvenanceEngine/MetricScraper/minimal_setup/node-exporter/daemonset.yaml
+kubectl create -f ../ProvenanceEngine/MetricScraper/minimal_setup/node-exporter/service.yaml
+
+# setup pgSQL & pgREST
+#kubectl apply -f ../ProvenanceEngine/database/pgSQL/db-persistent-volume.yaml --namespace $namespace2
+#kubectl apply -f ../ProvenanceEngine/database/pgSQL/db-volume-claim.yaml --namespace $namespace2
+#kubectl apply -f ../ProvenanceEngine/database/pgSQL/db-configmap.yaml --namespace $namespace2
+#kubectl apply -f ../ProvenanceEngine/database/pgSQL/db-deployment.yaml --namespace $namespace2
+#kubectl apply -f ../ProvenanceEngine/database/pgSQL/db-service.yaml --namespace $namespace2
+
+#echo -e "------database service and pods started------\n"
+
+#kubectl apply -f ../ProvenanceEnginedatabase/pgREST/postgrest-deployment.yaml --namespace $namespace2
+#kubectl apply -f ../ProvenanceEngine/database/pgREST/postgrest-service.yaml --namespace $namespace2
+
+#echo -e "------postGREST service and pods started------ \n"
+# load input data sets in cws-namespace onto management pod
+
+#kubectl cp inputs/rnaseq $namespace/management:/input/
 kubectl cp inputs/sarek $namespace/management:/input/
 #kubectl cp inputs/chipseq $namespace/management:/input/
 #kubectl cp inputs/atacseq $namespace/management:/input/
@@ -19,10 +48,14 @@ kubectl cp inputs/sarek $namespace/management:/input/
 #kubectl cp inputs/eager $namespace/management:/input/
 
 kubectl cp setup/nanoseqPatch.diff $namespace/management:/input/
+
+# run commands.sh on management pod
 kubectl cp setup/commands.sh $namespace/management:/input/
 
+# apply cws service-account
 kubectl apply -f setup/accounts.yaml --namespace $namespace
 
+# label cluster-nodes
 kubectl label nodes minikube cwsscheduler=true
 kubectl label nodes minikube-m02 minikube-m03 minikube-m04 cwsexperiment=true
 
